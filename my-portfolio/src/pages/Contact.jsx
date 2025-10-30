1af7627b3bc0f0bb7b13458e44ce8c3b8c7799f1
Frank Spitzock <157513226+FSpitzock@users.noreply.github.com>
2025-10-29 22:11:12 -0400

    contact page updated with conditional rendering

--- my-portfolio/src/pages/Contact.jsx
+++ my-portfolio/src/pages/Contact.jsx
@@ -1,7 +1,38 @@
-function Contact () {
-    return(
-        <p> need somthing in here</p>
-    );
+import { useState } from "react";
+
+function Contact() {
+  const [showResume, setShowResume] = useState(false); // false = show contact info
+
+  return (
+    <>
+      <h2>Contact & Resume</h2>
+      
+      <div style={{ marginBottom: "1rem" }}>
+        <button onClick={() => setShowResume(false)}>Contact Info</button>
+        <button onClick={() => setShowResume(true)}>Resume Info</button>
+      </div>
+
+      
+      {showResume ? (
+        <div className="card">
+          <h3>Resume</h3>
+          <p>Download my resume <a href="./resume.html" target="_blank" rel="noopener noreferrer">here</a>.</p>
+         </div>
+      ) : (
+        <div className="card">
+          <h3>Contact Info</h3>
+          <p>Email: fspitzock@yahoo.com</p>
+          <p>Phone: 239-233-0032</p>
+          <p>
+            GitHub Profile:{" "}
+            <a href="https://github.com/FSpitzock" target="_blank" rel="noopener noreferrer">
+              FSpitzock
+            </a>
+          </p>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
